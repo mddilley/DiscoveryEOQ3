a7ada49d81f65e717d538dda15762614616ccb95
Mike Dilley <mike.dilley@gmail.com>
2019-10-18 12:27:59 -0500

    Add axios, get 311 data in useEffect

--- screens/PotholeIndexScreen.js
+++ screens/PotholeIndexScreen.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ScrollView, StyleSheet } from "react-native";
 import { ExpoLinksView } from "@expo/samples";
+import axios from "axios";
 
 export default function PotholeIndexScreen() {
+  useEffect(() => {
+    axios
+      .get("https://data.austintexas.gov/resource/xwdj-i9he.json")
+      .then(res => {
+        console.log(res);
+      });
+  }, []);
+
   return (
     <ScrollView style={styles.container}>
       {/**
        * Go ahead and delete ExpoLinksView and replace it with your content;
        * we just wanted to provide you with some helpful links.
        */}
-      <ExpoLinksView />
+      {/* <ExpoLinksView /> */}
     </ScrollView>
   );
 }
